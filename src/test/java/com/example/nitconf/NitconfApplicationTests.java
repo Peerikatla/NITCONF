e255dcde25f5c4a3f201bd76ec82fb752824d46e
Kavuru Naveen Kumar <129659666+The-Naveen@users.noreply.github.com>
2024-01-20 14:50:25 +0530

    Initial

---
+++ src/test/java/com/example/nitconf/NitconfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nitconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitconfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

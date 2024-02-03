944ce9eb5b278e89055a58539e095519bbf32aca
Vijayalakshmi2805 <155812943+Vijayalakshmi2805@users.noreply.github.com>
2024-02-03 14:44:07 +0530

    Update UserRepository.java

--- src/main/java/com/example/Repository/UserRepository.java
+++ src/main/java/com/example/Repository/UserRepository.java
@@ -1,14 +1,14 @@
 package com.example.Repository;
 
 import com.example.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByUsername(String username);
 
-    User findByEmailId(String emailId);
+    //User findByEmailId(String emailId);
 
 }

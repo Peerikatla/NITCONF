444f8908ddedb0a1c144bc5256df995810b58690
Peerikatla <155981406+Peerikatla@users.noreply.github.com>
2024-02-03 14:29:27 +0530

    Update HomeController.java

--- src/main/java/com/example/Controller/HomeController.java
+++ src/main/java/com/example/Controller/HomeController.java
@@ -3,10 +3,11 @@ package com.example.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
-		@GetMapping("/home")
-		public String home() {
-			return "home";
-		}
+    @GetMapping("/")
+    public String home(){
+        return "loginPage";
+    }
 }

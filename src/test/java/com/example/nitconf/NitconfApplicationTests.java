1164fa456959e56214ea97113f04c06bbd0eb4a5
Vijayalakshmi Ganipisetty <155812943+Vijayalakshmi2805@users.noreply.github.com>
2024-03-19 14:32:50 +0530

    Update NitconfApplicationTests.java

--- src/test/java/com/example/nitconf/NitconfApplicationTests.java
+++ src/test/java/com/example/nitconf/NitconfApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.nitconf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = NitconfApplicationTests.class)
 class NitconfApplicationTests {
 
 	@Test

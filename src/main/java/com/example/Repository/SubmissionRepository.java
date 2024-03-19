b3d5148caa1e75db4bcc144579568e5af6e4fadb
TheNaveen <naveenkumar_b210560cs@nitc.ac.in>
2024-03-19 10:18:07 +0530

    added more conditions in api

--- src/main/java/com/example/Repository/SubmissionRepository.java
+++ src/main/java/com/example/Repository/SubmissionRepository.java
@@ -1,7 +1,5 @@
 package com.example.Repository;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -19,5 +17,5 @@ public interface SubmissionRepository extends CrudRepository<Submission, Integer
      * @param submissionId the ID of the submission
      * @return an Optional containing the found submission, or an empty Optional if not found
      */
-    Optional<Submission> findBysubmissionId(int submissionId);
+    Submission findBysubmissionId(int submissionId);
 }
